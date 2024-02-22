9947211c7e50231c8a5c10c4749b8cecdf680b05
IrinaZinchenko <zinahohol@gmail.com>
2024-02-22 20:35:13 +0300

    made a primary version with an input, a button and list of tasks all in one component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const App = () => {
-  return (
-    <div className='app'>My first React App</div>
-  )
+export default function App() {
+  const [task, setTask] = useState("");
+  const [tasksList, setTasksList] = useState([]);
+
+  const handleChange = (event) => {
+    setTask(event.target.value);
+    console.log(event.target.value);
+  };
+
+  const addTask = (task) => {
+    setTasksList([...tasksList, task]);
+    console.log(tasksList);
+  };
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    addTask(task);
+    console.log(task);
+  };
+
+    return (
+      <div className = "todo-app">
+        <div className = "todo-container">
+          <input className = "todo-input" type = "text" onChange = {handleChange}/>
+          <button className = "todo-btn" onClick = {handleClick}>Add Task</button>
+        </div>
+        <div>{tasksList.map((elem, index) => <div key = {index}>{elem}</div>)}</div>
+      </div>
+    );
 }
-
-export default App;
