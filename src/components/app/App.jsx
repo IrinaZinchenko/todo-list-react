b44fe43c8fe514e2405cf38d0861955d4165d3ec
IrinaZinchenko <zinahohol@gmail.com>
2024-02-27 13:19:26 +0300

    warnings fixed

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -30,19 +30,23 @@ export default function App() {
     <div className = "todo-app">
       <div className = "todo-container">
         <div className = "inputs-container">
-          <label className = "form-label" for = "task-name-input">Task</label>
+          <label className = "form-label" htmlFor = "task-name-input">Task</label>
           <input className = "task-name-input form-control mb-2" type = "text" id = "task-name-input" onChange = {handleChangeName}/>
-          <label className = "form-label" for = "task-description-input">Description</label>
+          <label className = "form-label" htmlFor = "task-description-input">Description</label>
           <input className = "task-description-input form-control mb-2" type = "text" id = "task-description-input" onChange = {handleChangeDescription}/>
         </div>
         <button className = "todo-btn btn btn-dark m-2" onClick = {handleClick}>Add Task</button>
       </div>
       <table>
-        <tr>
-          <th>Task</th>
-          <th>Description</th>
-        </tr>
-        {tasksList.map((elem, index) => <tr key = {index}><td>{elem.name}</td><td>{elem.description}</td></tr>)}
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+          <tbody>
+            {tasksList.map((elem, index) => <tr key = {index}><td>{elem.name}</td><td>{elem.description}</td></tr>)}
+          </tbody>
       </table>
     </div>
   );
